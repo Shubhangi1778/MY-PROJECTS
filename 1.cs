e17b0bec7a883fe430448ad175d43803aa8d42ef
Shubhangi1778 <105271206+Shubhangi1778@users.noreply.github.com>
2022-05-10 13:06:05 +0530

    SHOPPING WEBSITE
    ITS A SHOPPING WEBSITE

---
+++ 1.cs
@@ -0,0 +1,24 @@
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400&display=swap');
+
+body{
+    font-family: 'Montserrat', sans-serif;
+}
+h2{
+    margin-left:20px;
+}
+.clothing-section{
+    margin-left:20px;
+    display:flex;
+}
+.accessories-section{
+    margin-left:20px;
+    display:flex;
+}
+.card{
+width:15%;
+padding:10px;
+}
+.card .image{
+    width:100%;
+    background-color:red;
+}
